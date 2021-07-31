ecfa3e4735f968561e10833003ec9cab1274a678
ryanregier <ryan.regier@my.wheaton.edu>
2021-07-30 19:30:43 -0500

    Fixed

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 BASE = "http://127.0.0.1:80/"
 data = [{"name": "Bitcoin", "asset_type": "Cryptocurrency", "ticker": "BTC"}, {"name": "Ethereum", "asset_type": "Cryptocurrency", "ticker": "ETH"}, {"name": "Doge Coin", "asset_type": "Cryptocurrency", "ticker": "DOGE"}, {"name": "Cardano", "asset_type": "Cryptocurrency", "ticker": "ADA"}, {"name": "SushiSwap", "asset_type": "Cryptocurrency", "ticker": "SUSHI"}]
-requests.patch(BASE + "market/0", {"name": "PENIS"})
+requests.patch(BASE + "market/0", {"name": "Test"})
 for i in range(len(data)):
     response = requests.put(BASE + "market/" + str(i), data[i])
     print(response.json())
